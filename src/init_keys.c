f165b61411251ae14aea54fbfa7792f8103ccaae
Tishj <t_b@live.nl>
2021-03-10 15:29:34 +0100

    init_keys line numbers normified

--- src/init_keys.c
+++ src/init_keys.c
@@ -6,18 +6,32 @@
 /*   By: tishj <tishj@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/03/09 11:38:54 by tishj         #+#    #+#                 */
-/*   Updated: 2021/03/09 14:57:52 by tishj         ########   odam.nl         */
+/*   Updated: 2021/03/10 15:28:17 by tishj         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <reins_int.h>
+
+static int	regular_ascii_keys(t_reins *reins)
+{
+	char	buff[2];
+
+	buff[0] = ' ';
+	buff[1] = '\0';
+	while (buff[0] <= 126)
+	{
+		if (!reins_key(reins, buff, key_regular))
+			return (0);
+		buff[0]++;
+	}
+	return (1);
+}
 
 static int	populate_keys_vector(t_reins* reins,
 		const t_keyf* functions,
 		const char **keycodes)
 {
 	size_t	i;
-	size_t	j;
 	char	tmp[6];
 
 	if (!vec_new(&reins->keys, sizeof(t_key)))
@@ -26,18 +40,8 @@ static int	populate_keys_vector(t_reins* reins,
 	while (functions[i])
 	{
 		util_bzero(tmp, 6);
-		if (functions[i] == key_regular)
-		{
-			j = 32;
-			util_strncpy(tmp, (char *)keycodes[i], 6);
-			while (j <= 126)
-			{
-				tmp[0] = (char)j;
-				if (!reins_key(reins, tmp, functions[i]))
-					return (0);
-				j++;
-			}
-		}
+		if (functions[i] == key_regular && !regular_ascii_keys(reins))
+			return (0);
 		else if (!reins_key(reins, (char *)keycodes[i], functions[i]))
 			return (0);
 		i++;
