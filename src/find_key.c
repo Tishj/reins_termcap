694c5fd2fb0c144d4ebb99c911a6fc0e6a13b512
Tishj <t_b@live.nl>
2021-03-13 21:37:21 +0100

    copy pasting functional

--- src/find_key.c
+++ src/find_key.c
@@ -6,7 +6,7 @@
 /*   By: tishj <tishj@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/03/07 21:22:14 by tishj         #+#    #+#                 */
-/*   Updated: 2021/03/10 14:33:04 by tishj         ########   odam.nl         */
+/*   Updated: 2021/03/13 21:36:54 by tbruinem      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,12 @@ ssize_t	find_key(t_reins *reins, char *keycode, size_t size)
 	while (i < reins->keys.size)
 	{
 		tmp = vec_getref(&reins->keys, i);
-		if (!util_strncmp(tmp->buf, keycode, size))
+		if (tmp->size > size)
+		{
+			i++;
+			continue ;
+		}
+		if (!util_strncmp(tmp->buf, keycode, tmp->size))
 			return (i);
 		i++;
 	}
